99dbdbe98cc685acc0a8386b49b44e229e0e30e5
Ubuntu <ubuntu@ip-172-31-9-234.ap-south-1.compute.internal>
2021-12-28 19:00:12 +0000

    Revert "fixed docker file"
    This reverts commit 64c2f4cb84d7a5b51b96acaf7ee17a0abc6642c0.
    commited without sitting username and password on ec2 machine, thus reverting back the Ocommit

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -38,7 +38,7 @@ class PredictionRequest(BaseModel):
     text: str
 
 
-prediction_pipeline = Prediction(model=model, tokenizer=tokenizer)
+prediction_pipeline = Prediction()
 
 
 @app.get("/ping")
